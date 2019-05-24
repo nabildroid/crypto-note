eb82b2c2c96da3c2d9df462175e2b0c6d67ad34b
nabil <pni20156789@gmail.com>
2019-05-24 15:54:01 +0100

    fixing last div removed
    the previous host was force injecting html ads to main application so i add javascript code for auto removing but in this new host there's no html ads the the code were removing  the member div

--- js/control.js
+++ js/control.js
@@ -7,21 +7,10 @@ var CRYPTO;
 var member;
 let  t;
 window.addEventListener("DOMContentLoaded",()=>{
-	Array.from(elm("body > div","",1)).pop().remove()
 	connect=new Ajax("https://pfalgeria2018.000webhostapp.com/note.php","POST");
 	CRYPTO=new Crypto;
 
 	fade(elm(".login"),1).then(()=>{
 		login=new Login(elm(".login"),connect);
 	});
-
-
-	// connect.send({},"GET").then(txt=>{
-	// 	db=txt.split("¤");
-	// 	return new Promise((r)=>setTimeout(r,1000));
-	// }).then(()=>{
-	// 	
-	// 	return fade(elm(".loader"),0);
-	// }).then(()=>);
-
 });
