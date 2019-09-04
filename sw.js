5e52600e9a8c6969b508cc1a32ff7a226c3830ed
nabildroid <pni20156789@gmail.com>
2019-09-04 15:30:59 +0100

    delete unnecessary javascript files from service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let version="1.4.9";
+let version="1.4.10";
 self.addEventListener("install",(event)=>{
 	event.waitUntil(
 		caches.open("note-"+version).then(async cach=>{
@@ -14,8 +14,6 @@ self.addEventListener("install",(event)=>{
 				"./js/note.js",
 				"./js/hashtag.js",
 				"./js/guess.js",
-				"./js/translate.js",
-				"./js/translate.json",
 				"./style/color.css",
 				"./style/phone.css",
 				"./style/style.css"
